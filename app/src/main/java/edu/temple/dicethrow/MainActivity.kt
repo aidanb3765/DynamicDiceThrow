03c825323c610275e3dfdaf07195b81b863c7014
aidanb3765 <aidan.blaney@temple.edu>
2024-10-30 15:27:42 -0400

    Added loop for each layout
    Added a loop to check if a landscape or portrait layout is called, then switch to that layout functionality using View ids

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -27,6 +28,12 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
             - Show only Button Fragment if portrait
             - show both fragments if Landscape
           */
+        // Loop to check container within activity_main layout
+        if (findViewById<View>(R.id.container2) != null) {
+            supportFragmentManager.beginTransaction().replace(R.id.container1, ButtonFragment()).replace(R.id.container2, DieFragment()).commit()
+        } else {
+            supportFragmentManager.beginTransaction().replace(R.id.container1, ButtonFragment()).commit()
+        }
     }
 
     /* TODO 2: switch fragments if portrait (no need to switch fragments if Landscape)
